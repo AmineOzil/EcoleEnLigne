1310f64560d36fed9a67756559b7700b7ab54d80
Amine aminbouali1995@hotmail.com <aminbouali1995@hotmail.com>
2020-04-07 17:12:26 +0200

    Email verification works

--- app/src/main/java/com/devmobile/ecoleenligne/BienvenueActivity.java
+++ app/src/main/java/com/devmobile/ecoleenligne/BienvenueActivity.java
@@ -2,13 +2,24 @@ package com.devmobile.ecoleenligne;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class BienvenueActivity extends AppCompatActivity {
-
+    Button start;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bienvenue);
+        start= findViewById(R.id.bienvenue_commencer);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signup=new Intent(BienvenueActivity.this,inscription.class);
+                startActivity(signup);
+            }
+        });
     }
 }
--- app/src/main/java/com/devmobile/ecoleenligne/inscription.java
+++ app/src/main/java/com/devmobile/ecoleenligne/inscription.java
@@ -1,14 +1,63 @@
 package com.devmobile.ecoleenligne;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class inscription extends AppCompatActivity {
-
+    Button suivant;
+    EditText email,password;
+    FirebaseAuth auth;
+    FirebaseUser user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inscription);
+        auth = FirebaseAuth.getInstance();
+
+        suivant = findViewById(R.id.signup_suivant);
+        email = findViewById(R.id.signup_mail);
+        password = findViewById(R.id.signup_mdp);
+        suivant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                auth.createUserWithEmailAndPassword(email.getText().toString().trim(),password.getText().toString().trim()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                 @Override
+                 public void onComplete(@NonNull Task<AuthResult> task) {
+                     if (task.isSuccessful()){
+                         user = auth.getCurrentUser();
+                         user.sendEmailVerification()
+                                 .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                     @Override
+                                     public void onComplete(@NonNull Task<Void> task) {
+                                         if (task.isSuccessful()) {
+                                             Toast.makeText(inscription.this,"Utilisateur ajouté veuillez vérifier votre mail",Toast.LENGTH_SHORT).show();
+                                         }
+                                         else {
+                                             Toast.makeText(inscription.this,task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+
+                                         }
+                                     }
+                                 });
+                     }else {
+                         Toast.makeText(inscription.this,task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                     }
+                 }
+             });
+            }
+        });
+
     }
 }
