6a901f5fe56208b7c48611a9b6ef0e6db2644e37
Amine aminbouali1995@hotmail.com <aminbouali1995@hotmail.com>
2020-05-08 03:26:01 +0200

    dashboard selon niveau

--- app/src/main/java/com/devmobile/ecoleenligne/DashboardFragment.java
+++ app/src/main/java/com/devmobile/ecoleenligne/DashboardFragment.java
@@ -1,18 +1,26 @@
 package com.devmobile.ecoleenligne;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 public class DashboardFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment_dashboard,container,false);
-        return view;
+        View view=inflater.inflate(R.layout.fragment_dashboard_ts,container,false);
+        switch (dashboard.niveau){
+            case "Terminal L": view=inflater.inflate(R.layout.fragment_dashboard_tl,container,false);
+                return view;
+            case "Seconde":   view=inflater.inflate(R.layout.fragment_dashboard_s,container,false);
+                return view;
+            case "Primaire": view=inflater.inflate(R.layout.fragment_dashboard_p,container,false);
+                return view;
+            default: return view;
+
+        }
     }
+
 }
